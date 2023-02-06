6f6969dc85b2640d84c68c26d21f59b578220858
wt128 <s1280120@u-aizu.ac.jp>
2023-02-07 08:23:01 +0900

    とりあえず、コンテナで動かせるようにしたり

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/wt128/taiyaki-blog
+
+go 1.18
---
+++ backend/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/extra/bundebug"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+	ctx := context.Background()
+
+	// Open a PostgreSQL database.
+	dsn := "postgres://postgres:@localhost:5432/test?sslmode=disable"
+	pgdb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+
+	// Create a Bun db on top of it.
+	db := bun.NewDB(pgdb, pgdialect.New())
+
+	// Print all queries to stdout.
+	db.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(true)))
+
+	var rnd float64
+
+	// Select a random number.
+	if err := db.NewSelect().ColumnExpr("random()").Scan(ctx, &rnd); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(rnd)
+}
